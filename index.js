f1a8f9cc099ba05986b4bfa6f46ebb09bca038ad
michaelroesinger <mroesinger@gmail.com>
2022-07-21 08:43:19 -0700

    commit tree b7a37c7191d0f7774242975513945390161611d5

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const employee = {
-    name: "Sam" ,
-    streetAddress: "11 Broadway",
-}
+  name: "Sam",
+  streetAddress: "11 Broadway",
+};
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return {...employee,[key]:value,}
-}        
+  return { ...employee, [key]: value };
+}
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee
+  employee[key] = value;
+  return employee;
 }
 function deleteFromEmployeeByKey(employee, key, value) {
-    return {...employee,[key]: value}
+  return { ...employee, [key]: value };
 }
 function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
-    delete employee[key];
-    return employee;
+  delete employee[key];
+  return employee;
 }
